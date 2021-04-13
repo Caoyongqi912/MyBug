5ed3dfd462c7cf526022dc50aa6973c0b5bb44b3
cyq <yongqi.cao@duobei.com>
2021-04-13 17:12:08 +0800

    add

--- COMMENT/sqlOpt.py
+++ COMMENT/sqlOpt.py
@@ -36,12 +36,13 @@ class SqlOpt:
             sql = f"SELECT * FROM {self.table} WHERE "
 
         for param in params:
-            s = " ".join([param.get("key"), param.get("condition"), '"' + str(param.get('val')) + '" ' + f"{opt} ".upper()])
+            s = " ".join(
+                [param.get("key"), param.get("condition"), '"' + str(param.get('val')) + '" ' + f"{opt} ".upper()])
             sql += s
-        sql = sql.strip(f"{opt} ".upper())
+        sql = sql.rstrip(f"{opt} ".upper())
         return self._doSelect(sql, limit)
 
-    def update(self, params: list, targets: list, opt: str = "and")->bool:
+    def update(self, params: list, targets: list, opt: str = "and") -> bool:
         """
         update table set title = 'test' where id =1 and p1
         :params: [{"key":"id",condition:[ ">"|"<"|"=" ],"val":"1"},]
@@ -59,9 +60,10 @@ class SqlOpt:
         sql = f"UPDATE {self.table} SET {tar} WHERE "
         if len(params) > 1:
             for param in params:
-                s = " ".join([param.get("key"), param.get("condition"), '"' + str(param.get('val')) + '" ' + f"{opt} ".upper()])
+                s = " ".join(
+                    [param.get("key"), param.get("condition"), '"' + str(param.get('val')) + '" ' + f"{opt} ".upper()])
                 sql += s
-            sql = sql.strip(f"{opt} ".upper())
+            sql = sql.rstrip(f"{opt} ".upper())
         else:
             param = params[0]
             s = " ".join([param.get("key"), param.get("condition"), '"' + str(param.get('val')) + '"'])
@@ -75,18 +77,53 @@ class SqlOpt:
             log.error(e)
             return False
 
-    def insert(self):
+    def insert(self, params: list):
         """
         insert into table values (val1,val2)
         insert into table (const1,const2) values (val1,val2)
+        :param [{"key":"name","val":"cyq"]
         :return:
         """
+        const = list()
+        values = list()
 
-    def delete(self):
+        for param in params:
+            const.append(param['key'])
+            values.append(f"'{str(param['val'])}'")
+        sql = f"INSERT INTO {self.table} ({','.join(const)}) VALUES ({','.join(values)})  "
+        try:
+            self.eng.execute(sql)
+            log.info(f"sql: {sql}")
+            return True
+        except Exception as e:
+            print(e)
+            log.error(e)
+            return False
+
+    def delete(self, params: list, opt: str = "") -> bool:
         """
         delete from table where k=v
+        :params: [{"key":"id",condition:[ ">"|"<"|"=" ],"val":"1"},]
+        :opt : AND OR None
         :return:
         """
+        sql = f"DELETE FROM {self.table} WHERE "
+        params = self._verify(params)
+        for param in params:
+            s = " ".join(
+                [param.get("key"), param.get("condition"), '"' + str(param.get('val')) + '" ' + f"{opt} ".upper()])
+            sql += s
+
+        sql = sql.rstrip(f"{opt} ".upper())
+
+        try:
+            self.eng.execute(sql)
+            log.info(f"sql: {sql}")
+            return True
+        except Exception as e:
+            print(e)
+            log.error(e)
+            return False
 
     def _doSelect(self, sql, limit) -> list:
         try:
@@ -117,6 +154,15 @@ if __name__ == '__main__':
     from Model.models import User
 
     put_targets = [{"key": "title", "val": "test"}, {"key": "level", "val": "p1"}]
-    put_opt =[{'key': 'id', 'condition': '=', 'val': 1}]
+    put_opt = [{'key': 'id', 'condition': '=', 'val': 1}]
+
+    delPar = [{"key": "id", "val": 1, "condition": "="}]
+    delOpt = "AND"
+
     # a = SqlOpt("bugs").select(opt, targets)
-    SqlOpt('bugs').update(put_opt, put_targets)
+    # SqlOpt('bugs').update(put_opt, put_targets)
+    # SqlOpt("bugs").delete(delPar)
+    import time
+
+    insertPar = [{"key": "name", "val": "test"}, {"key": "create_time", "val": time.time()}]
+    SqlOpt("project").insert(insertPar)
